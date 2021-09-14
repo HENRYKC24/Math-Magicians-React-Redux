26c75bf8f5965cd968d6ab698b0d4f6e95f1ce43
HENRY-KC <henrykc24@yahoo.com>
2021-09-14 14:14:28 +0100

    Add and implement calculator class

---
+++ src/components/Calculator.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Table from './Table';
+
+export default class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const tableData = [
+      [0],
+      ['AC', '+/-', '%', '+'],
+      [7, 8, 9, `${String.fromCharCode(215)}`],
+      [4, 5, 6, '-'],
+      [1, 2, 3, '+'],
+      [0, `${String.fromCharCode(183)}`, '='],
+    ];
+
+    return (
+      <Table tableData={tableData} />
+    );
+  }
+}
