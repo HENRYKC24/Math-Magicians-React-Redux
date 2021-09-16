fdfaa5eda9ee87f04c4e0230aeb21bf83bd0c9a3
HENRY-KC <henrykc24@yahoo.com>
2021-09-16 12:02:55 +0100

    Refactor TalbeData component to use function component

--- src/components/TableData.js
+++ src/components/TableData.js
@@ -1,41 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import propTypes from 'prop-types';
 import styles from './tableData.module.css';
 
-class TableData extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const { td } = styles;
-    const {
-      data,
-      handleClick,
-      otherProps,
-    } = this.props;
-    const { next, operation, total } = otherProps;
-    return data === 0 ? (
-      <td className={td}>
-        <div>
-          <span>{total}</span>
-          <span>{operation}</span>
-          <span>{next}</span>
-        </div>
-      </td>
-    ) : (
-      <td className={td}>
-        <div
-          aria-hidden
-          onClick={() => handleClick(data)}
-        >
-          {data}
-        </div>
-      </td>
-    );
-  }
-}
+const TableData = (props) => {
+  const { td } = styles;
+  const { data, handleClick, otherProps } = props;
+  const { next, operation, total } = otherProps;
+  return data === 0 ? (
+    <td className={td}>
+      <div>
+        <span>{total}</span>
+        <span>{operation}</span>
+        <span>{next}</span>
+      </div>
+    </td>
+  ) : (
+    <td className={td}>
+      <div aria-hidden onClick={() => handleClick(data)}>
+        {data}
+      </div>
+    </td>
+  );
+};
 
 TableData.propTypes = {
   data: propTypes.oneOfType([propTypes.string, propTypes.number]),
