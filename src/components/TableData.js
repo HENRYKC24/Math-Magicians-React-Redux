6102e60219473179e0954165663099d7dbb74b73
HENRY-KC <henrykc24@yahoo.com>
2021-09-16 16:42:45 +0100

    Remove multiple span elements and make them a single component

--- src/components/TableData.js
+++ src/components/TableData.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import styles from './tableData.module.css';
+import TableScreen from './TableScreen';
 
 const TableData = (props) => {
   const { td } = styles;
   const { data, handleClick, otherProps } = props;
   const { next, operation, total } = otherProps;
+  const mapArray = [total, operation, next];
   return data === 0 ? (
     <td className={td}>
       <div>
-        <span>{total}</span>
-        <span>{operation}</span>
-        <span>{next}</span>
+        {mapArray.map((data) => <TableScreen key={Math.random()} data={data} />)}
       </div>
     </td>
   ) : (
