635720a3b243c917e859d212ff3fd33d05b40e07
HENRY-KC <henrykc24@yahoo.com>
2021-09-16 12:03:20 +0100

    Refactor TableRow component to use function component

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,37 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import propTypes from 'prop-types';
 import styles from './TableRow.module.css';
 import TableData from './TableData';
 
-class TableRow extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const TableRow = (props) => {
+  const { tr } = styles;
+  const { rowData, handleClick, otherProps } = props;
 
-  render() {
-    const { tr } = styles;
-    const {
-      rowData,
-      handleClick,
-      otherProps,
-    } = this.props;
-
-    return (
-      <tr className={tr}>
-        {rowData.map((data) => (
-          <TableData
-            key={Math.random()}
-            stateObj={this.state}
-            data={data}
-            handleClick={handleClick}
-            otherProps={otherProps}
-          />
-        ))}
-      </tr>
-    );
-  }
-}
+  return (
+    <tr className={tr}>
+      {rowData.map((data) => (
+        <TableData
+          key={Math.random()}
+          data={data}
+          handleClick={handleClick}
+          otherProps={otherProps}
+        />
+      ))}
+    </tr>
+  );
+};
 
 TableRow.propTypes = {
   rowData: propTypes.instanceOf(Array).isRequired,
