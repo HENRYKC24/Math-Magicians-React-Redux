e869ad6f41131ec1b801283a933b05e7e0456c3a
HENRY-KC <henrykc24@yahoo.com>
2021-09-22 15:57:33 +0100

    Moved component to the right channel

---
+++ src/components/Calculator.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import calculate from '../logic/calculator';
+import Table from './Table';
+
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (data) => {
+    setState((obj) => {
+      const newObject = calculate(state, data);
+      return {
+        ...obj,
+        ...newObject,
+      };
+    });
+  };
+
+  const tableData = [
+    [0],
+    ['AC', '+/-', '%', `${String.fromCharCode(247)}`],
+    ['7', '8', '9', 'x'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '+'],
+    ['0', '.', '='],
+  ];
+
+  return (
+    <Table otherProps={state} handleClick={handleClick} tableData={tableData} />
+  );
+};
+
+export default Calculator;
