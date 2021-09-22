2535941035401a70ae27fb14f8a5b72717b3da9b
HENRY-KC <henrykc24@yahoo.com>
2021-09-22 20:07:10 +0100

    Create a component for all pages routing

---
+++ src/pages/AppRoutes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Calculator2 from './Calculator2';
+import Navbar from '../components/Navbar';
+import Home from './Home';
+import Quotes from './Quotes';
+import PageNotFound from './PageNotFound';
+
+const AppRoutes = () => (
+  <>
+    <Navbar />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/calculator">
+        <Calculator2 />
+      </Route>
+      <Route path="/quotes">
+        <Quotes />
+      </Route>
+
+      <Route path="*">
+        <PageNotFound />
+      </Route>
+    </Switch>
+  </>
+);
+
+export default AppRoutes;
