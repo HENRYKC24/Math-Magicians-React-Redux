6c5e35c741f5f9c2336fdfb18db852a341819b84
HENRY-KC <henrykc24@yahoo.com>
2021-09-22 15:57:05 +0100

    Update import url

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
-import Calculator from './components/jsModules/Calculator';
+import Calculator from './components/Calculator';
 
 ReactDOM.render(
   <React.StrictMode>
