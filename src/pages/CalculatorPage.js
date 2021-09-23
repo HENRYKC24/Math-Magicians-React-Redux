c6d8f1ac1086efbbd24f31a9df61cc86bc4c1848
HENRY-KC <henrykc24@yahoo.com>
2021-09-23 07:43:51 +0100

    Use more descriptive name for components

---
+++ src/pages/CalculatorPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Calculator from '../components/Calculator';
+
+const Calculator2 = () => (
+  <div className="calculator-page">
+    <p className="lets">Let&apos;s do some math!</p>
+    <Calculator />
+  </div>
+);
+
+export default Calculator2;
